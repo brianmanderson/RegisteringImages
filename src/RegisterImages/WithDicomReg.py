2b12d2a6f407fc505d987e463d61e37aa1b3f9b7
markba122@gmail.com <bmanderson@mdanderson.org>
2020-11-12 16:35:58 -0600

    default dicom_registration to None, allows for resmpaling with Identify Affine Transform

--- src/RegisterImages/WithDicomReg.py
+++ src/RegisterImages/WithDicomReg.py
@@ -5,7 +5,7 @@ import pydicom
 import numpy as np
 
 
-def register_images_with_dicom_reg(fixed_image, moving_image, dicom_registration, min_value=-1000):
+def register_images_with_dicom_reg(fixed_image, moving_image, dicom_registration=None, min_value=-1000):
     """
     :param fixed_image: The fixed image
     :param moving_image: The moving image
@@ -15,14 +15,15 @@ def register_images_with_dicom_reg(fixed_image, moving_image, dicom_registration
     """
     assert type(fixed_image) is sitk.Image, 'You need to pass a SimpleITK image as fixed_image'
     assert type(moving_image) is sitk.Image, 'You need to pass a SimpleITK image as moving_image'
-    assert type(dicom_registration) is pydicom.dataset.FileDataset, 'Pass dicom_registration as ' \
-                                                                    'pydicom.read_file(reg_file)'
-    registration_matrix = np.asarray(dicom_registration.RegistrationSequence[-1].MatrixRegistrationSequence[-1].
-                                     MatrixSequence[-1].FrameOfReferenceTransformationMatrix)[:-4]
-    registration_matrix = np.transpose(np.reshape(registration_matrix, (3, 4)))
-    registration_matrix[-1, :] *= -1
+    assert type(dicom_registration) is pydicom.dataset.FileDataset \
+           or dicom_registration is None, 'Pass dicom_registration as pydicom.read_file(reg_file) or None'
     affine_transform = sitk.AffineTransform(3)
-    affine_transform.SetParameters(registration_matrix.flatten())
+    if dicom_registration is not None:
+        registration_matrix = np.asarray(dicom_registration.RegistrationSequence[-1].MatrixRegistrationSequence[-1].
+                                         MatrixSequence[-1].FrameOfReferenceTransformationMatrix)[:-4]
+        registration_matrix = np.transpose(np.reshape(registration_matrix, (3, 4)))
+        registration_matrix[-1, :] *= -1
+        affine_transform.SetParameters(registration_matrix.flatten())
     moving_resampled = sitk.Resample(moving_image, fixed_image, affine_transform, sitk.sitkLinear, min_value,
                                      moving_image.GetPixelID())
     return moving_resampled
