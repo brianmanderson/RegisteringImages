faf70b0f02e81bf1d15d40a779d282aa978bc256
markba122@gmail.com <bmanderson@mdanderson.org>
2020-11-13 13:55:44 -0600

    properly works now, had to take the inverse of the matrix.. don't push moving to fixed, pull moving to fixed*. Major help from Bastien Rigaud

--- src/RegisterImages/WithDicomReg.py
+++ src/RegisterImages/WithDicomReg.py
@@ -5,12 +5,14 @@ import pydicom
 import numpy as np
 
 
-def register_images_with_dicom_reg(fixed_image, moving_image, dicom_registration=None, min_value=-1000):
+def register_images_with_dicom_reg(fixed_image, moving_image, dicom_registration=None, min_value=-1000,
+                                   method=sitk.sitkLinear):
     """
     :param fixed_image: The fixed image
     :param moving_image: The moving image
     :param dicom_registration: the DICOM Registration file
     :param min_value: Value to put as background in resampled image
+    :param method: interpolating method, recommend sitk.sitkLinear for images and sitk.sitkNearestNeighbor for masks
     :return:
     """
     assert type(fixed_image) is sitk.Image, 'You need to pass a SimpleITK image as fixed_image'
@@ -20,11 +22,11 @@ def register_images_with_dicom_reg(fixed_image, moving_image, dicom_registration
     affine_transform = sitk.AffineTransform(3)
     if dicom_registration is not None:
         registration_matrix = np.asarray(dicom_registration.RegistrationSequence[-1].MatrixRegistrationSequence[-1].
-                                         MatrixSequence[-1].FrameOfReferenceTransformationMatrix)[:-4]
-        registration_matrix = np.transpose(np.reshape(registration_matrix, (3, 4)))
-        registration_matrix[-1, :] *= -1
-        affine_transform.SetParameters(registration_matrix.flatten())
-    moving_resampled = sitk.Resample(moving_image, fixed_image, affine_transform, sitk.sitkLinear, min_value,
+                                         MatrixSequence[-1].FrameOfReferenceTransformationMatrix).reshape((4, 4))
+        registration_matrix = np.linalg.inv(registration_matrix)
+        affine_transform.SetMatrix(registration_matrix[:3, :3].ravel())
+        affine_transform.SetTranslation(registration_matrix[:3, -1])
+    moving_resampled = sitk.Resample(moving_image, fixed_image, affine_transform, method, min_value,
                                      moving_image.GetPixelID())
     return moving_resampled
 
